63c7bb1dfd904ebc93a7aec251989035ef6a4975
Agustin Isasa <tin.isasa@gmail.com>
2019-11-20 11:54:34 -0300

    Add slider interactions

--- js/index.js
+++ js/index.js
@@ -1,66 +1,3 @@
-var minutes = $( '#set-time' ).val();
-
-var target_date = new Date().getTime() + ((minutes * 60 ) * 1000); // set the countdown date
-var time_limit = ((minutes * 60 ) * 1000);
-//set actual timer
-setTimeout(
-  function() 
-  {
-    alert( 'done' );
-  }, time_limit );
-
-var days, hours, minutes, seconds; // variables for time units
-
-var countdown = document.getElementById("tiles"); // get tag element
-
-getCountdown();
-
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-  
-if ( seconds_left >= 0 ) {
-  console.log(time_limit);
-   if ( (seconds_left * 1000 ) < ( time_limit / 2 ) )  {
-     $( '#tiles' ).removeClass('color-full');
-     $( '#tiles' ).addClass('color-half');
-
-		} 
-    if ( (seconds_left * 1000 ) < ( time_limit / 4 ) )  {
-    	$( '#tiles' ).removeClass('color-half');
-    	$( '#tiles' ).addClass('color-empty');
-    }
-  
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-		 
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-		  
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + hours + ":</span><span>" + minutes + ":</span><span>" + seconds + "</span>"; 
-  
-
-  
-}
-   
-  
-  
-}
-
-function pad(n) {
-	return (n < 10 ? '0' : '') + n;
-}
-
-
-
 // slider
 
 var rangeSlider = function(){
@@ -77,8 +14,25 @@ var rangeSlider = function(){
   
       range.on('input', function(){
         $(this).next(value).html(this.value);
+        console.log(this.value.toString());
+        if ($(this).siblings(":last").is($('#text-change'))) {
+          console.log('cambio tamaño')
+          $('#text-change').css('font-size', this.value.toString() + "px");
+        }
+        else if ($(this).siblings(":last").is($('#luminance-change'))) {
+          console.log('cambio luminancia')
+          $('#luminance-change').css('color', "hsl(0, 0%, " + this.value.toString() + "%");
+        }
+        else if ($(this).siblings(":last").is($('#hue-change'))) {
+          console.log('cambio matiz')
+          $('#hue-change').css('color', "hsl(" + this.value + ", 50%, 80%");
+        }
+        $(this).html(this.value);
       });
     });
   };
   
   rangeSlider();
+
+
+  
